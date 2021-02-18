069f81a2b29ea03079bf5e7160de3954fd07d142
Jack Chen <zcemwjc@ucl.ac.uk>
2021-02-19 06:57:08 +0800

    choropleth_map_populations

--- matplotlib_line_graph.py
+++ matplotlib_line_graph.py
@@ -4,5 +4,16 @@ import matplotlib.dates as mdates
 
 %matplotlib inline
 
-# Read the data for the population (F001) into a data frame skipping the second heading row
-population_data = pd.read_csv('parliamentary-constituency-profiles-data.csv', usecols=["POPULATION"], skiprows=[1])
+# Read the data for the population and crime into a data frame skipping the second heading row
+data = pd.read_csv('parliamentary-constituency-profiles-data.csv', usecols=["F100", "F010"], skiprows=[1])
+
+# Create a new DataFrame with the required Date column
+population = data.loc[:,['F010']]
+crime = data.loc[:,['F100']]
+
+fig, axes = plt.subplots(figsize=(12, 6))
+
+plt.plot(population, crime, 'o')
+axes.set_xlabel('Population Density')
+axes.set_ylabel('Crime Rate')
+axes.set_title('The graph of Population Density against Crime Rate')
