0851311d349e2daee0ef75f63f6a4b57e2d1592f
Jack Chen <zcemwjc@ucl.ac.uk>
2021-03-01 07:41:34 +0800

    choropleth_map_populations

--- matplotlib_line_graph.py
+++ matplotlib_line_graph.py
@@ -5,11 +5,11 @@ import matplotlib.dates as mdates
 %matplotlib inline
 
 # Read the data for the population and crime into a data frame skipping the second heading row
-data = pd.read_csv('parliamentary-constituency-profiles-data.csv', usecols=["F100", "F010"], skiprows=[1])
+data = pd.read_csv('parliamentary-constituency-profiles-data.csv', usecols=["Crime Rate", "F010"], skiprows=[1])
 
 # Create a new DataFrame with the required Date column
 population = data.loc[:,['F010']]
-crime = data.loc[:,['F100']]
+crime = data.loc[:,['Crime Rate']]
 
 fig, axes = plt.subplots(figsize=(12, 6))
 
---
+++ plotly_dash_express.py
@@ -0,0 +1,51 @@
+import pandas as pd
+import plotly.express as px
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import dash_bootstrap_components as dbc
+
+# Read the data
+data = pd.read_csv('parliamentary-constituency-profiles-data.csv', skiprows=[1])
+
+# Create the bubble graph
+fig = px.scatter(data,
+                 x="POPULATION",
+                 y="F100",
+                 size="pop",
+                 color="continent",
+                 hover_name="GEO_LABEL",
+                 log_x = True,
+                 size_max=60000,
+                 )
+
+fig.update_layout(
+    title='Crime Rate v.s. Population',
+    xaxis=dict(
+        title='Population',
+    ),
+    yaxis=dict(
+        title='Crime Rate)',
+    ),
+)
+
+app = dash.Dash(_name_)
+
+# Create the app layout
+app.layout = html.Div(children=[
+    html.H1(children='Visualization of Population in Greater London'),
+
+    html.Div(children='''
+        A scatter plot of population in Greater London
+    '''),
+
+    dcc.Graph(
+        id='example-graph',
+        figure= fig,
+        style={'width': '200vh', 'height': '90vh'}
+
+    )
+])
+
+if _name_ == '_main_':
+    app.run_server(debug=True)
